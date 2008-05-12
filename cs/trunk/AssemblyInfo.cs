a16669e8f9ce544a71f475919eebcc8234eecc72
grammarian <grammarian@0bec5ed8-b53f-49e6-9885-ce7bc93af311>
2008-05-12 19:38:33 +0000

    Moved remotely

---
+++ cs/trunk/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Information about this assembly is defined by the following
+// attributes.
+//
+// change them to the information which is associated with the assembly
+// you compile.
+
+[assembly: AssemblyTitle("ObjectListViewDemo")]
+[assembly: AssemblyDescription("A simple demonstration of how easy an ObjectListView is to use.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Bright Ideas Software")]
+[assembly: AssemblyProduct("ObjectListViewDemo")]
+[assembly: AssemblyCopyright("2006-2008 All Rights Reserved")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// This sets the default COM visibility of types in the assembly to invisible.
+// If you need to expose a type to COM, use [ComVisible(true)] on that type.
+[assembly: ComVisible(false)]
+
+// The assembly version has following format :
+//
+// Major.Minor.Build.Revision
+//
+// You can specify all values by your own or you can build default build and revision
+// numbers with the '*' character (the default):
+
+[assembly: AssemblyVersion("1.11.*")]
+[assembly: AssemblyFileVersionAttribute("1.11")]
+[assembly: System.CLSCompliant(true)]
