99ccf5947633b4e620627c9c6a3571ee8eecd8d4
Grammarian <Grammarian@0bec5ed8-b53f-49e6-9885-ce7bc93af311>
2008-11-29 14:23:34 +0000

    - Changed license to GPL v3, to be consistent with ObjectListView. - Added more compatibility methods/Properties - Changed version number

--- ListViewPrinter/Properties/AssemblyInfo.cs
+++ ListViewPrinter/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ListViewPrinter")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A class to take a ListView or ObjectListView and make it into a beatiful report")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Bright Ideas Software")]
 [assembly: AssemblyProduct("ListViewPrinter")]
 [assembly: AssemblyCopyright("Copyright ©  2008")]
 [assembly: AssemblyTrademark("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.*")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
