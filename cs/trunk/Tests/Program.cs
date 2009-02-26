b25bfc358bf6b18fd7ef1e78cc0e1b124144542e
grammarian <grammarian@0bec5ed8-b53f-49e6-9885-ce7bc93af311>
2009-02-26 06:41:59 +0000

    - Updated tests to allow for events on virtual lists

--- cs/trunk/Tests/Program.cs
+++ cs/trunk/Tests/Program.cs
@@ -21,47 +21,47 @@ namespace BrightIdeasSoftware.Tests
 		/// <summary>
 		/// Program entry point.
 		/// </summary>
-//        [STAThread]
-//        private static void Main(string[] args)
-//        {
-////			Application.EnableVisualStyles();
-////			Application.SetCompatibleTextRenderingDefault(false);
-////			Application.Run(new MainForm());
-//            MyGlobals g = new MyGlobals();
-//            g.RunBeforeAnyTests();
-
-//            TestAspectSetting t = new TestAspectSetting();
-//            t.Init();
-//            //t.InitEachTest();
-//            t.TestSimpleMethod();
-
-//            g.RunAfterAnyTests();
-//        }
-
         [STAThread]
         private static void Main(string[] args)
         {
-            int start = Environment.TickCount;
-            int iterations = 1000;
+            //			Application.EnableVisualStyles();
+            //			Application.SetCompatibleTextRenderingDefault(false);
+            //			Application.Run(new MainForm());
+            MyGlobals g = new MyGlobals();
+            g.RunBeforeAnyTests();
 
-            Person person1 = new Person("name", "occupation", 100, DateTime.Now, 1.0, true, "  photo  ", "comments");
-            Person2 person2 = new Person2("name", "occupation", 100, DateTime.Now, 1.0, true, "  photo  ", "comments");
+            TestOlvCheckBoxes t = new TestOlvCheckBoxes();
+            t.Init();
+            t.InitEachTest();
+            t.TestCheckStatePutter();
 
-            OLVColumn column1 = new OLVColumn("ignored", "CulinaryRating");
-            OLVColumn column2 = new OLVColumn("ignored", "BirthDate.Year.ToString.Length");
-            OLVColumn column3 = new OLVColumn("ignored", "Photo.ToString.Trim");
-
-            for (int i = 0; i < iterations; i++) {
-                column1.GetValue(person1);
-                column1.GetValue(person2);
-                column2.GetValue(person1);
-                column2.GetValue(person2);
-                column3.GetValue(person1);
-                column3.GetValue(person2);
-            }
-
-            Console.WriteLine("Elapsed time: {0}ms", Environment.TickCount-start);
+            g.RunAfterAnyTests();
         }
+
+        //[STAThread]
+        //private static void Main(string[] args)
+        //{
+        //    int start = Environment.TickCount;
+        //    int iterations = 1000;
+
+        //    Person person1 = new Person("name", "occupation", 100, DateTime.Now, 1.0, true, "  photo  ", "comments");
+        //    Person2 person2 = new Person2("name", "occupation", 100, DateTime.Now, 1.0, true, "  photo  ", "comments");
+
+        //    OLVColumn column1 = new OLVColumn("ignored", "CulinaryRating");
+        //    OLVColumn column2 = new OLVColumn("ignored", "BirthDate.Year.ToString.Length");
+        //    OLVColumn column3 = new OLVColumn("ignored", "Photo.ToString.Trim");
+
+        //    for (int i = 0; i < iterations; i++) {
+        //        column1.GetValue(person1);
+        //        column1.GetValue(person2);
+        //        column2.GetValue(person1);
+        //        column2.GetValue(person2);
+        //        column3.GetValue(person1);
+        //        column3.GetValue(person2);
+        //    }
+
+        //    Console.WriteLine("Elapsed time: {0}ms", Environment.TickCount-start);
+        //}
 
         // Base line: Elapsed time: 204ms
     }
