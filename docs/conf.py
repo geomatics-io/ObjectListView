eaa9a5740c1cdd45218860a49e67309034f275e3
grammarian <grammarian@mycompany.com>
2010-08-28 05:26:45 +0000

    - Update ready for v2.4.1
    git-svn-id: https://svn.code.sf.net/p/objectlistview/code/cs/trunk@480 0bec5ed8-b53f-49e6-9885-ce7bc93af311

--- docs/conf.py
+++ docs/conf.py
@@ -44,9 +44,9 @@ copyright = '2006-2010, Phillip Piper'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '2.4'
+version = '2.4.1'
 # The full version, including alpha/beta/rc tags.
-release = '2.4'
+release = '2.4.1'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
