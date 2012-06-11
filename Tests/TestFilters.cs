6ba82fc90ba41a421f3a483d9fa6d246a491cba5
grammarian <grammarian@0bec5ed8-b53f-49e6-9885-ce7bc93af311>
2012-06-11 04:30:09 +0000

    - Added new clustering tests - Added notification tests

--- Tests/TestFilters.cs
+++ Tests/TestFilters.cs
@@ -346,4 +346,156 @@ namespace BrightIdeasSoftware.Tests
             this.olv = MyGlobals.mainForm.treeListView1;
         }
     }
+
+    [TestFixture]
+    public class TestDateClusteringStrategy {
+
+        readonly DateTime DATE1 = new DateTime(1998, 11, 30, 22, 23, 24);
+        readonly DateTime DATE2 = new DateTime(1999, 12, 31, 22, 23, 24);
+
+        [Test]
+        public void Test_Construction_Empty() {
+            DateTimeClusteringStrategy strategy = new DateTimeClusteringStrategy();
+            strategy.Column = new OLVColumn();
+            strategy.Column.AspectGetter = delegate(object x) { return DATE2; };
+            object result = strategy.GetClusterKey(null);
+
+            Assert.AreEqual(new DateTime(1999, 12, 1), result);
+        }
+
+        [Test]
+        public void Test_Construction_WithPortions() {
+            DateTimeClusteringStrategy strategy = new DateTimeClusteringStrategy(DateTimePortion.Hour | DateTimePortion.Minute, "HH:mm");
+            strategy.Column = new OLVColumn();
+            strategy.Column.AspectGetter = delegate(object x) { return DATE1; };
+            object result = strategy.GetClusterKey(null);
+
+            Assert.AreEqual(new DateTime(1, 1, 1, 22, 23, 0), result);
+        }
+
+        [Test]
+        public void Test_Extracting_FromNull() {
+            DateTimeClusteringStrategy strategy = new DateTimeClusteringStrategy(DateTimePortion.Hour | DateTimePortion.Minute, "HH:mm");
+            strategy.Column = new OLVColumn();
+            strategy.Column.AspectGetter = delegate(object x) { return null; };
+            object result = strategy.GetClusterKey(null);
+            Assert.IsNull(result);
+        }
+
+        [Test]
+        public void Test_GetClusterDisplayLabel_Plural() {
+            DateTimeClusteringStrategy strategy = new DateTimeClusteringStrategy(DateTimePortion.Hour | DateTimePortion.Minute, "HH:mm");
+            strategy.Column = new OLVColumn();
+            strategy.Column.AspectGetter = delegate(object x) { return DATE1; };
+            ICluster cluster = new Cluster(strategy.GetClusterKey(null));
+            cluster.Count = 2;
+            string result = strategy.GetClusterDisplayLabel(cluster);
+            Assert.AreEqual("22:23 (2 items)", result);
+        }
+
+        [Test]
+        public void Test_GetClusterDisplayLabel_Singular() {
+            DateTimeClusteringStrategy strategy = new DateTimeClusteringStrategy(DateTimePortion.Year | DateTimePortion.Month, "MM-yy");
+            strategy.Column = new OLVColumn();
+            strategy.Column.AspectGetter = delegate(object x) { return DATE1; };
+            ICluster cluster = new Cluster(strategy.GetClusterKey(null));
+            cluster.Count = 1;
+            string result = strategy.GetClusterDisplayLabel(cluster);
+            Assert.AreEqual("11-98 (1 item)", result);
+        }
+
+        [Test]
+        public void Test_GetClusterDisplayLabel_NullValue() {
+            DateTimeClusteringStrategy strategy = new DateTimeClusteringStrategy(DateTimePortion.Year | DateTimePortion.Month, "HH:mm");
+            strategy.Column = new OLVColumn();
+            strategy.Column.AspectGetter = delegate(object x) { return DATE1; };
+            ICluster cluster = new Cluster(null);
+            cluster.Count = 1;
+            string result = strategy.GetClusterDisplayLabel(cluster);
+            Assert.AreEqual(ClusteringStrategy.NULL_LABEL + " (1 item)", result);
+        }
+    }
+
+    [TestFixture]
+    public class TestFlagClusteringStrategy {
+
+        [Test]
+        public void Test_EnumConstruction_Values() {
+            FlagClusteringStrategy strategy = new FlagClusteringStrategy(typeof(TestFlagEnum));
+            Assert.AreEqual(4, strategy.Values.Length);
+            Assert.Contains(2, strategy.Values);
+            Assert.Contains(4, strategy.Values);
+            Assert.Contains(8, strategy.Values);
+            Assert.Contains(16, strategy.Values);
+        }
+
+        [Test]
+        public void Test_EnumConstruction_Labels() {
+            FlagClusteringStrategy strategy = new FlagClusteringStrategy(typeof(TestFlagEnum));
+            Assert.AreEqual(4, strategy.Labels.Length);
+            Assert.Contains("FlagValue1", strategy.Labels);
+            Assert.Contains("FlagValue2", strategy.Labels);
+            Assert.Contains("FlagValue3", strategy.Labels);
+            Assert.Contains("FlagValue4", strategy.Labels);
+        }
+
+        [Test]
+        public void Test_GetClusterKey() {
+            FlagClusteringStrategy strategy = new FlagClusteringStrategy(typeof(TestFlagEnum));
+            strategy.Column = new OLVColumn();
+            strategy.Column.AspectGetter = delegate(object x) { return TestFlagEnum.FlagValue1 | TestFlagEnum.FlagValue4; };
+            object result = strategy.GetClusterKey(null);
+            Assert.IsInstanceOf<IEnumerable>(result);
+            Assert.AreEqual(2, ((ICollection)result).Count);
+            Assert.Contains((ulong)TestFlagEnum.FlagValue1, result as ICollection);
+            Assert.Contains((ulong)TestFlagEnum.FlagValue4, result as ICollection);
+        }
+
+        [Test]
+        public void Test_GetClusterKey_ZeroValue() {
+            FlagClusteringStrategy strategy = new FlagClusteringStrategy(typeof(TestFlagEnum));
+            strategy.Column = new OLVColumn();
+            strategy.Column.AspectGetter = delegate(object x) { return 0; };
+            object result = strategy.GetClusterKey(null);
+            Assert.IsInstanceOf<IEnumerable>(result);
+            Assert.AreEqual(0, ((ICollection)result).Count);
+        }
+
+        [Test]
+        public void Test_GetClusterKey_NonNumericValue() {
+            FlagClusteringStrategy strategy = new FlagClusteringStrategy(typeof(TestFlagEnum));
+            strategy.Column = new OLVColumn();
+            strategy.Column.AspectGetter = delegate(object x) { return "not number"; };
+            object result = strategy.GetClusterKey(null);
+            Assert.IsInstanceOf<IEnumerable>(result);
+            Assert.AreEqual(0, ((ICollection)result).Count);
+        }
+
+        [Test]
+        public void Test_GetClusterKey_NonConvertibleValue() {
+            FlagClusteringStrategy strategy = new FlagClusteringStrategy(typeof(TestFlagEnum));
+            strategy.Column = new OLVColumn();
+            strategy.Column.AspectGetter = delegate(object x) { return new object(); };
+            object result = strategy.GetClusterKey(null);
+            Assert.IsInstanceOf<IEnumerable>(result);
+            Assert.AreEqual(0, ((ICollection)result).Count);
+        }
+
+        [Test]
+        public void Test_GetClusterDisplayLabel() {
+            FlagClusteringStrategy strategy = new FlagClusteringStrategy(typeof(TestFlagEnum));
+            ICluster cluster = new Cluster(TestFlagEnum.FlagValue2);
+            cluster.Count = 2;
+            string result = strategy.GetClusterDisplayLabel(cluster);
+            Assert.AreEqual("FlagValue2 (2 items)", result);
+        }
+
+        [Flags]
+        private enum TestFlagEnum {
+            FlagValue1 = 2,
+            FlagValue2 = 4,
+            FlagValue3 = 8,
+            FlagValue4 = 16
+        }
+    }
 }
