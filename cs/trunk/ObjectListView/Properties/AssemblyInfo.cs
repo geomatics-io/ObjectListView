2c9ac5cd97eca424e966a6e175c46b6afd3b98f1
grammarian <grammarian@0bec5ed8-b53f-49e6-9885-ce7bc93af311>
2015-07-03 15:23:17 +0000

    - Update version to 2.9.0 alpha

--- cs/trunk/ObjectListView/Properties/AssemblyInfo.cs
+++ cs/trunk/ObjectListView/Properties/AssemblyInfo.cs
@@ -30,6 +30,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.8.1.*")]
-[assembly: AssemblyFileVersion("2.8.1.0")]
+[assembly: AssemblyVersion("2.9.0.*")]
+[assembly: AssemblyFileVersion("2.9.0.0")]
+[assembly: AssemblyInformationalVersion("2.9.0-alpha")]
 [assembly: System.CLSCompliant(true)]
