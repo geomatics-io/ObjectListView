43232be2223142a58d65cb4e84d135ca9675433c
Christian Junk <christian.junk@woodplc.com>
2019-09-24 12:11:14 +0200

    New release.

--- ObjectListView/Properties/AssemblyInfo.cs
+++ ObjectListView/Properties/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.9.1.*")]
-[assembly: AssemblyFileVersion("2.9.1.0")]
-[assembly: AssemblyInformationalVersion("2.9.1")]
+[assembly: AssemblyVersion("2.9.2.*")]
+[assembly: AssemblyFileVersion("2.9.2.0")]
+[assembly: AssemblyInformationalVersion("2.9.2")]
 [assembly: System.CLSCompliant(true)]
